63777dd62b9787425b382c714cacf314b04d8f9a
Trumanibrilliance360 <trumaniacedevior@gmail.com>
2026-04-16 01:55:08 -0400

    Create GodTru.jsx component file

---
+++ src/components/GodTru.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const GodTru = () => {
+    return (
+        <div>
+            <h1>Welcome to GodTru Component</h1>
+            <p>This is your GodTru component, built with love!</p>
+        </div>
+    );
+};
+
+export default GodTru;
